bfb58c8ab45d9148cbed3ea33c6d45406743fa50
riley <rileywestonmiller@outlook.com>
2023-07-16 17:30:27 -0700

    iMpRoVeD aLgOrItHm

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 function sarcasm(line){
-    line.toLowerCase();
+    line = line.toLowerCase();
     const res = [];
+    let wordIdx = 0;
     for (let i = 0; i < line.length; i++) {
         let code = line.charCodeAt(i);
-        if(code > 96 && code < 123 && i % 2 === 0) code -= 32;
+        // if(code > 96 && code < 123 && i % 2 === 0) code -= 32;
+        if(i === 0 || line[i-1] === ' ') wordIdx = 0;
+        if(code > 96 && code < 123 && wordIdx % 2 === 1) {
+            code -= 32;
+        }
+        wordIdx++;
         res.push(code);
     }
     return String.fromCharCode(...res);
@@ -25,3 +31,5 @@ function main(){
 }
 
 main();
+
+console.log(sarcasm("look bitch fucking work"));
